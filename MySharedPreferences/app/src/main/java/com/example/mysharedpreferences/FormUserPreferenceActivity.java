587fbeebef2679346923fb5e9736a2d8d73070a2
gemaannisa <gemaannisa05@gmail.com>
2020-12-21 06:55:46 +0700

    Read Write

---
+++ MySharedPreferences/app/src/main/java/com/example/mysharedpreferences/FormUserPreferenceActivity.java
@@ -0,0 +1,13 @@
+package com.example.mysharedpreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormUserPreferenceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_user_preference);
+    }
+}
---
+++ MySharedPreferences/app/src/main/java/com/example/mysharedpreferences/UserModel.java
@@ -0,0 +1,4 @@
+package com.example.mysharedpreferences;
+
+public class UserModel {
+}
